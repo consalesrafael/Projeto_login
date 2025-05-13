2462bfe2d44f44f65afa6290875ebdc1ff771552
consalesrafael <consalesrafael63@gmail.com>
2025-05-13 19:42:40 -0300

    sada

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const Express = require("express")
-const app = new Express()
+const express = require("express");
+const path = require("path");
+const app = express();
 
+// Configura o middleware para arquivos estáticos (CSS, JS, imagens)
+app.use(express.static(path.join(__dirname, "public")));
 
+// Rota principal ("/") redireciona para /login
+app.get("/", (req, res) => {
+    res.redirect("/login"); // Redireciona para a rota /login
+});
 
+// Rota /login → envia o arquivo login.html
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "login.html"));
+});
 
-app.listen(3000, (req, res)=>{
-    console.log("Servidor aberto ena porta 3000")
-})
-
+app.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+});
